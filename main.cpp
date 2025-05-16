1c01bb297102742de9a7bd41ea377dc79db3583e
David Soriano <davidsoriano707@gmail.com>
2025-05-15 21:28:11 -0700

    Main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "graph.h"
 
 int main() {
-    /*
+
     Graph g(5);
 
     g.addEdge(0, 1, 2);
@@ -14,21 +14,8 @@ int main() {
     g.addEdge(3, 4, 9);
 
     g.primMST();
-    */
-    Graph g(7);
 
-    g.addEdge(0, 1, 2);
-    g.addEdge(0, 3, 1);
-    g.addEdge(1, 2, 3);
-    g.addEdge(1, 3, 3);
-    g.addEdge(1, 4, 6);
-    g.addEdge(2, 4, 5);
-    g.addEdge(2, 5, 4);
-    g.addEdge(3, 4, 7);
-    g.addEdge(3, 6, 8);
-    g.addEdge(4, 5, 2);
-    g.addEdge(5, 6, 1);
-    g.primMST();
+
 
 
 
